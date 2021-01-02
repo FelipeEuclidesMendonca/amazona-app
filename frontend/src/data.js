4024a2fa4d17a57da47398c17cd75e6fe37abb45
felipeuclides <felipeuclides@yahoo.com.br>
2021-01-02 15:52:52 -0300

    Share code on Github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Camisa Nike Slim',
+            category: 'Shirts',
+            image: 'images/p1.jpg',
+            price: 120.00,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'Produto original de alta qualidade'
+        },
+        {
+            _id:'2',
+            name: 'Camisa Lacoste',
+            category: 'Shirts',
+            image: 'images/p2.jpg',
+            price: 99.00,
+            brand: 'Lacoste',
+            rating: 3.5,
+            numReviews: 20,
+            description: 'Produto original de alta qualidade'
+        },
+        {
+            _id:'3',
+            name: 'Camisa Dudalina Slim',
+            category: 'Shirts',
+            image: 'images/p3.jpg',
+            price: 150.00,
+            brand: 'Dudalina',
+            rating: 5,
+            numReviews: 8,
+            description: 'Produto original de alta qualidade'
+        },
+        {
+            _id:'4',
+            name: 'Calsa Jeans Hofman White',
+            category: 'Pants',
+            image: 'images/p4.jpg',
+            price: 159.99,
+            brand: 'Hofman',
+            rating: 4,
+            numReviews: 12,
+            description: 'Produto original de alta qualidade'
+        },
+        {
+            _id:'5',
+            name: 'Calsa Jeans Hofman Green',
+            category: 'Pants',
+            image: 'images/p5.jpg',
+            price: 159.99,
+            brand: 'Hofman',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'Produto original de alta qualidade'
+        },
+        {
+            _id:'6',
+            name: 'Calsa Jeans Hofman Purple',
+            category: 'Pants',
+            image: 'images/p6.jpg',
+            price: 159.99,
+            brand: 'Hofman',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Produto original de alta qualidade'
+        }
+    ]
+}
+
+export default data
